cda218556fe5187bcb0b63eecde6d159c97ed231
bambus <132773043+bambus80@users.noreply.github.com>
2025-04-07 09:07:46 +0200

    use pygame instead of audioplayer

--- src/player.py
+++ src/player.py
@@ -1,4 +1,4 @@
-from audioplayer import AudioPlayer
+import pygame
 import audio_metadata
 import os
 import random
@@ -11,62 +11,63 @@ class PlaylistController:
         self.song_list: list[str] = os.listdir(self.path)
         print(f"Total songs: {len(self.song_list)} | {self.song_list}")
 
-        self.player: AudioPlayer | None = None
         self.metadata: audio_metadata.Format | None = None
         self.is_playing: bool = False
         self.mode: str | None = None
+
+        pygame.mixer.init()
         self._open(self.idx)
 
     def _open(self, idx: int) -> None:
-        print(f"Opening {self.song_list[idx]}... ", end="")
-        path = f"{self.path}/{self.song_list[idx]}"
+        path = os.path.join(os.getcwd(), self.path, self.song_list[idx])
+        print(f"Opening {path}... ", end="")
         try:
             if self.is_playing:
-                self.is_playing = False
-                self.player.close()
+                pygame.mixer.music.stop()
             self.metadata = audio_metadata.load(path)
-            self.player = AudioPlayer(path)
+            pygame.mixer.music.load(path)
         except Exception as e:
             print(f"FAILED! {e}")
+            return
         print("OK")
 
     def play(self) -> None:
         if not self.is_playing:
-            print("Resuming currnet song")
-            self.player.resume()
-        else:
             print("Playing song")
-            self.player.play()
+            pygame.mixer.music.play()
+        else:
+            print("Resuming song")
+            pygame.mixer.music.unpause()
         self.is_playing = True
 
     def pause(self) -> None:
         print("Pausing song")
-        self.player.pause()
+        pygame.mixer.music.pause()
         self.is_playing = False
 
     def stop(self) -> None:
-        if not self.player:
-            print("Player already closed, no need to stop")
-        print("Stopping song")
-        self.player.stop()
+        if not pygame.mixer.music.get_busy():
+            print("No song playing, nothing to stop")
+        else:
+            print("Stopping song")
+            pygame.mixer.music.stop()
         self.idx = 0
-        # self._open(0)
 
     def next(self) -> None:
-        self.player.close()
+        print("Next song")
+        pygame.mixer.music.stop()
         match self.mode:
             case "repeat":
-                if self.idx == len(self.song_list) - 1:
-                    self.idx = 0
-                else:
-                    self.idx += 1
+                self.idx = 0 if self.idx == len(self.song_list) - 1 else self.idx + 1
             case "shuffle":
                 self.idx = random.randrange(0, len(self.song_list))
             case _:
                 if self.idx == len(self.song_list) - 1:
                     self.stop()
+                    return
                 self.idx += 1
         self._open(self.idx)
+        self.play()
 
     def get_stream_info(self) -> dict:
         stream_info = self.metadata.streaminfo
@@ -76,3 +77,6 @@ class PlaylistController:
             "rate": stream_info.get("sample_rate", "--"),
             "channels": stream_info.get("channels", "--")
         }
+
+    def set_volume(self, volume: float) -> None:
+        pygame.mixer.music.set_volume(volume)
